6d1acdb3340fb3e424cb875517094a06cd8d0711
Aashish Panthi <aashishpanthi11@gmail.com>
2024-02-16 16:05:07 +0545

    complete service section horizontal scrolling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center min-h-[115px] shadow-md px-2 lg:px-0 sticky top-0 bg-[#fff] z-50">
+    <nav className="flex items-center min-h-[100px] md:min-h-[115px] shadow-md px-2 lg:px-0 sticky top-0 bg-[#fff] z-50">
       <div className="flex items-center justify-between mx-auto max-w-7xl w-full">
         <Link href="/" passHref>
           <Image
@@ -11,12 +11,12 @@ const Navbar = () => {
             alt="logo"
             width={150}
             height={100}
-            className="cursor-pointer object-contain w-auto h-auto"
+            className="cursor-pointer object-contain w-auto h-[65px] md:h-[90px]"
           />
         </Link>
 
         <Link href="/contact" passHref>
-          <button className="px-10 py-4 bg-[#2D763A] text-white hover:shadow-md transition duration-300">
+          <button className="px-6 py-3 md:px-10 md:py-4 bg-[#2D763A] text-white hover:shadow-md transition duration-300">
             Contact Us
           </button>
         </Link>
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,53 +1,78 @@
 "use client";
 import Image from "next/image";
+import { useRef } from "react";
 
 import { FiArrowRight } from "react-icons/fi";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+// data
+import data from "@/data/services.json";
 
 const Services = () => {
+  const targetRef = useRef<HTMLDivElement | null>(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  });
+
+  const x = useTransform(scrollYProgress, [0, 1], ["10%", "-85%"]); // Adjust the range and values here
+
   return (
-    <section className="w-full min-h-[700px] bg-[#000] p-4 py-10 my-5">
-      <h1 className="text-6xl text-[#2D763A] text-center uppercase font-semibold mt-8">
-        Services
-      </h1>
+    <section
+      ref={targetRef}
+      className="w-full min-h-[250vh] bg-[#000] p-4 py-10 my-5"
+    >
+      <div className="sticky top-[125px] md:top-[140px]">
+        <h1 className="text-2xl md:text-4xl lg:text-6xl text-[#2D763A] text-center uppercase font-semibold mt-8">
+          Services
+        </h1>
 
-      <div className="flex items-center justify-evenly my-8">
-        <div className="overflow-hidden w-full h-[400px] md:w-[400px] lg:h-[550px] lg:w-[520px] relative rounded-tr-[100px] flex justify-center">
-          <Image
-            src="/assets/services/service-1.jpeg"
-            width={400}
-            height={400}
-            layout="responsive"
-            alt="service"
-            className="object-cover min-w-full min-h-full"
-          />
+        <div className="my-8 max-w-7xl mx-auto overflow-x-hidden">
+          <motion.div className="flex items-center gap-20" style={{ x }}>
+            {data.map((service) => (
+              <div
+                key={service.id}
+                className="overflow-hidden w-full h-[400px] md:min-w-[400px] md:w-[400px] lg:h-[550px] lf:min-w-[550px] lg:w-[550px] relative rounded-tr-[100px] flex justify-center"
+              >
+                <Image
+                  src={service.image}
+                  width={400}
+                  height={400}
+                  layout="responsive"
+                  alt="service"
+                  className="object-cover min-w-full min-h-full"
+                />
 
-          <div
-            className="absolute bottom-0 h-full w-full flex items-end justify-center"
-            style={{
-              background:
-                "linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1))",
-              pointerEvents: "none",
-            }}
-          >
-            <div className="text-white w-[80%]">
-              <h1 className="text-4xl font-[500]">Build with us</h1>
-              <p className="text-[16px] leading-[24px] mt-2">
-                You need to move faster. You have napkin sketches that need to
-                be fleshed out and realized. You need to reinvent your business
-                to compete.
-              </p>
+                <div
+                  className="absolute top-0 h-full w-full left-0"
+                  style={{
+                    background:
+                      "linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1))",
+                    pointerEvents: "none",
+                  }}
+                ></div>
 
-              <p className="text-[16px] leading-[24px] mt-2">
-                We bring startup speed and agility, and enterprise quality and
-                scale. We'd love to learn and share more.
-              </p>
+                <div className="absolute bottom-0 w-full flex items-end justify-center">
+                  <div className="text-white w-[85%]">
+                    <h1 className="text-2xl md:text-4xl font-[500]">
+                      {service.title}
+                    </h1>
+                    {service.description &&
+                      service.description.length > 0 &&
+                      service.description.map((desc, index) => (
+                        <p className="text-[14px] md:text-[16px] leading-[24px] mt-2">
+                          {desc}
+                        </p>
+                      ))}
 
-              <button className="text-[#2D763A] text-[16px] hover:text-[#fff] uppercase font-[600] hover:opacity-[.95] transition duration-300 rounded-md mt-2 mb-5 flex items-center">
-                Order now
-                <FiArrowRight className="ml-1" />
-              </button>
-            </div>
-          </div>
+                    <button className="text-[#2D763A] text-[14px] md:text-[16px] hover:text-[#fff] uppercase font-[600] hover:opacity-[.95] transition duration-300 rounded-md mt-2 mb-5 flex items-center">
+                      {service.cta_txt}
+                      <FiArrowRight className="ml-1" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </motion.div>
         </div>
       </div>
     </section>
